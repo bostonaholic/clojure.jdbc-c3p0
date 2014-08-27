df4b401a30756ed858aa21047969fcab4897484d
Andrey Antukh <niwi@niwi.be>
2014-08-27 23:12:01 +0200

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.2.0"
-  :description "clojure.jdbc extension for c3p0 (a mature, highly concurrent JDBC
-Connection pooling library)."
-  :url "http://github.com/niwibe/clojure.jdbc"
+(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.3.0"
+  :description "c3p0, a mature, highly concurrent JDBC connection pooling library for clojure.jdbc"
+  :url "http://github.com/niwibe/clojure.jdbc-c3p0"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
