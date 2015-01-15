77d32630e270cf008ab088b9fef7411add26700a
Andrey Antukh <niwi@niwi.be>
2015-01-15 10:43:12 +0100

    Remove explicit dependency to clojure.jdbc.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.3.1"
+(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.3.2"
   :description "c3p0, a mature, highly concurrent JDBC connection pooling library for clojure.jdbc"
   :url "http://github.com/niwibe/clojure.jdbc-c3p0"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojure.jdbc "0.3.1"]
                  [com.mchange/c3p0 "0.9.5-pre9"]]
 
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.176"]]}
